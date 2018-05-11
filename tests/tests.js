d812857459a8802f52de8091fd0d66a39acb89d6
luyuzhanshi <badams2@mail.mvnu.edu>
2018-05-11 09:39:03 -0400

    Initial commit

---
+++ tests/tests.js
@@ -0,0 +1,12 @@
+var CalculatorApp = require('../calculator/app.js')
+var CalculatorAppTests = function(){
+  this['calculator can store numbers'] = function(){
+    var calculatorApp = new CalculatorApp()
+    var expected = 0
+    calculatorApp.determineAction
+    var actual = calculatorApp.storedNumber
+    var result = actual == expected
+    return result
+  }
+}
+module.exports = CalculatorAppTests
