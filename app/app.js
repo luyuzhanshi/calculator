138b84d59066ef4fe0d45ac5c3f0794890c84b7d
luyuzhanshi <badams2@mail.mvnu.edu>
2018-05-11 10:02:29 -0400

    test 2
    test to see if calculator can store operators
    results: passed

--- app/app.js
+++ app/app.js
@@ -1,17 +1,23 @@
-var CalculatorApp = function(defaultInput){
-
-  this.appSetup = function(defaultInput){
-  if(!defaultInput){
-    defaultInput = {}
-  }
-    if (!defaultInput.storedNumber){
-      defaultInput.storedNumber = ''
+  var CalculatorApp = function(defaultInput){
+    this.appSetup = function(defaultInput){
+    if(!defaultInput){
+      defaultInput = {}
     }
-    this.storedNumber = defaultInput.storedNumber
+      if (!defaultInput.storedNumber){
+        defaultInput.storedNumber = ''
+      }
+      this.storedNumber = defaultInput.storedNumber
+    }
+    this.determineAction = function(input){
+      var inputIsNotNumber = input == 'x' || input == '+' || input == '/' || input == '-' || input == '='
+      if (inputIsNotNumber){
+
+      }
+      if (!inputIsNotNumber){
+        this.storedNumber += input
+      }
+    }
+    this.appSetup(defaultInput)
   }
-
-  this.appSetup(defaultInput)
-
-}
 
 module.exports = CalculatorApp
