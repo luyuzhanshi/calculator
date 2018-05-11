47eae86cd648f062c529ed07f546e791ac92d4c4
luyuzhanshi <badams2@mail.mvnu.edu>
2018-05-11 10:06:57 -0400

    test 3
    calculator can detect equals
    results: passed

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-  var CalculatorApp = function(defaultInput){
+var CalculatorApp = function(defaultInput){
     this.appSetup = function(defaultInput){
     if(!defaultInput){
       defaultInput = {}
@@ -6,18 +6,39 @@
       if (!defaultInput.storedNumber){
         defaultInput.storedNumber = ''
       }
+      if (!defaultInput.leftOperand){
+        defaultInput.leftOperand = ''
+      }
+      if (!defaultInput.rightOperand){
+        defaultInput.rightOperand = ''
+      }
       this.storedNumber = defaultInput.storedNumber
+      this.leftOperand = defaultInput.leftOperand
+      this.rightOperand = defaultInput.rightOperand
     }
     this.determineAction = function(input){
       var inputIsNotNumber = input == 'x' || input == '+' || input == '/' || input == '-' || input == '='
+      var inputIsEquals = input == '='
+
       if (inputIsNotNumber){
 
+        if (!inputIsEquals){
+          this.leftOperand = this.storedNumber
+          this.storedNumber = ''
+        }
+
+        if (inputIsEquals){
+          this.rightOperand = this.storedNumber
+          this.storedNumber = ''
+        }
+
       }
+
       if (!inputIsNotNumber){
         this.storedNumber += input
       }
+
     }
     this.appSetup(defaultInput)
   }
-
 module.exports = CalculatorApp
